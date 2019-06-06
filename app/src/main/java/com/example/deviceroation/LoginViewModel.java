76246a3fb800703edd27997988424f85017b8f45
nandajit <nandajitnath.ghy@hotmail.com>
2019-06-06 09:17:10 +0530

    login view binding

--- app/src/main/java/com/example/deviceroation/LoginViewModel.java
+++ app/src/main/java/com/example/deviceroation/LoginViewModel.java
@@ -1,6 +1,33 @@
 package com.example.deviceroation;
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 
 public class LoginViewModel extends BaseObservable {
+    private String userName;
+    private String password;
+
+    @Bindable
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        notifyPropertyChanged(BR.password);
+    }
+
+    @Bindable
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+        notifyPropertyChanged(BR.userName);
+    }
+
+    public void login(){
+
+    }
 }
